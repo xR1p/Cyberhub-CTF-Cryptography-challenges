960bc0d237d389e48fe66b77b14b191fa0b759cb
Dark_/script <54118024+D4rKOmaR@users.noreply.github.com>
2021-10-11 14:39:18 -0400

    Update Theweakmaster.py

--- Theweakmaster.py
+++ Theweakmaster.py
@@ -5,8 +5,8 @@
 
 
 from Crypto.Cipher import DES
-c = bytes.fromhex("7b43fb178c42f2bb5bb7c97d7294824fb62f7fd58e03fb1beac64d66cf5e376b")
+flag = bytes.fromhex("7b43fb178c42f2bb5bb7c97d7294824fb62f7fd58e03fb1beac64d66cf5e376b")
 key = b"\x01\x01\x01\x01\x01\x01\x01\x01"
 cipher = DES.new(key, DES.MODE_ECB)
-print(cipher.decrypt(c))
+print(cipher.decrypt(flag))
 #flag is CYBERHUB{345y_45_12345_67890_:D}
